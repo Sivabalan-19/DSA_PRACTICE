fcadc3ff234b111f7b24060da6cf56c5e919b1f7
Sivabalan-19 <pmsiva.1906@gmail.com>
2026-04-17 14:34:16 +0530

    insertion sort

--- Sorting/insertionSort.java
+++ Sorting/insertionSort.java
@@ -1,29 +1,28 @@
-class bubbleSort {
-
+public class insertionSort {
     public static void main(String[] args) {
 
-        int temp = 0;
-        int[] nums = { 6, 5, 2, 8, 9, 4 };
+        int[] nums = { 6, 5, 2, 8, 3, 7 };
         int n = nums.length;
         System.out.println("Array Before sorting");
         for (int i = 0; i < n; i++) {
             System.out.print(nums[i] + " ");
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (nums[j] > nums[j + 1]) {
-                    temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                }
+        for (int i = 1; i < n; i++) {
+            int key = nums[i];
+            int j = i - 1;
+            while (j >= 0 && nums[j] > key) {
+                nums[j + 1] = nums[j];
+                j--;
             }
+            nums[j + 1] = key;
         }
-        
+
         System.out.println();
         System.out.println("Array After sorting");
         for (int i = 0; i < n; i++) {
             System.out.print(nums[i] + " ");
         }
     }
+
 }
