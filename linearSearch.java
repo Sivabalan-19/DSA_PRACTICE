ab505a056c1f8a4aadf224c23c6480a14a7e56c8
Sivabalan-19 <pmsiva.1906@gmail.com>
2026-04-17 11:52:30 +0530

    Linear search

---
+++ linearSearch.java
@@ -0,0 +1,23 @@
+public class linearSearch {
+    public static int search(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 5, 7, 3, 5, 6, 7, 4 };
+        int target = 3;
+
+        int result = search(arr, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index " + result);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
